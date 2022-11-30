98e8d6775e0cd299cb17345d1831d44bf2d017e9
NTK <38352784+NTK1313@users.noreply.github.com>
2022-11-30 18:58:36 +0900

    株価表をLINE通知するように改修実施

--- define.py
+++ define.py
@@ -11,9 +11,12 @@ def get_share_info_list(info_list,period_day,frequency_day):
 	share_list = []
 	symbol_data_list = []
 	for code in info_list:
-	    share_list.append(share.Share(str(code) + '.T'))
-	for share_info in share_list:
-		symbol_data_list.append(get_share_info(share_info,period_day,frequency_day))
+		share_info = dict()
+		share_info["code"] = code
+		share_info["shareCode"] = (share.Share(str(code) + '.T'))
+		share_list.append(share_info)
+	for info in share_list:
+		symbol_data_list.append(get_share_info(info,period_day,frequency_day))
 	return symbol_data_list
 
 # 株価情報をYahooファイナンスから取得
@@ -21,7 +24,7 @@ def get_share_info_list(info_list,period_day,frequency_day):
 def get_share_info(info,period_day,frequency_day):
 	try:
 		# print(info)
-		symbol_data = info.get_historical(share.PERIOD_TYPE_DAY,
+		symbol_data = info["shareCode"].get_historical(share.PERIOD_TYPE_DAY,
 										period_day,
 										share.FREQUENCY_TYPE_DAY,
 										frequency_day)
@@ -33,6 +36,7 @@ def get_share_info(info,period_day,frequency_day):
 		# volume：出来高
 
 		# print(symbol_data.keys())
+		symbol_data["code"] = int(info["code"])
 	except YahooFinanceError as e:
 		print(e.message)
 		sys.exit(1)
@@ -59,6 +63,7 @@ def create_graph_data(data_list,code_list,data_period):
 			if str(price) != 'None':
 				# 小数点第2位を四捨五入
 				price_str = Decimal(str(price)).quantize(Decimal('0.1'), rounding=ROUND_HALF_UP)
+				#price_str = str(math.floor(price))
 				# print('取得日時：' + str(time) + ' ' +'株価(JPY)：' + str(price_str))
 				graph_x.append(str(datetime_formatted))
 				graph_y.append((price_str))
@@ -81,10 +86,17 @@ def line_graph():
 
 # LINE通知
 # 引数（通知メッセージ）
-def send_line_notify(notification_message):
+def send_line_notify(notification_message,file):
 	# LINEアクセストークン
     line_notify_token = 'PgZYKr6HE5OSxgio7gOXYEHFSCEct71dbAAetmP2BVf'
     line_notify_api = 'https://notify-api.line.me/api/notify'
     headers = {'Authorization': f'Bearer {line_notify_token}'}
     data = {'message': f'message: {notification_message}'}
-    requests.post(line_notify_api, headers = headers, data = data)
+	# 添付ファイル存在チェック
+    if len(file) == 0:
+        print('file is not exists')
+        requests.post(line_notify_api, headers = headers, data = data)
+    else:
+        print('file is exists')
+        files = {'imageFile': file}
+        requests.post(line_notify_api, headers = headers, data = data,files=files)
--- share_info.py
+++ share_info.py
@@ -4,12 +4,17 @@
 # 直近1週間のグラフ
 
 import define
-from datetime import datetime
+import numpy as np
+import numpy.random as random
+import matplotlib.pyplot as plt
+import io
+import datetime
 from decimal import Decimal, ROUND_HALF_UP
+from colorama import Fore, Back, Style
 
 # 4220 リケンテクノス
 # 4503 アステラス製薬
-# SPK
+# 7466 SPK
 # 8306 三菱UFL
 # 9432 NTT
 code_list = ['4220','4503','7466','8306','9432']
@@ -23,18 +28,19 @@ symbol_data_list = []
 # close：終値
 # volume：出来高
 EOL = '\r\n'
-SPECE = ' '
+SPACE = ' '
 name = None
-message = EOL + 'おはようございます。' + EOL + '最新の株価情報を送信します。' + EOL + 'ご確認ください。' + EOL +'-----以下、株価情報--------'
+message = EOL + 'おはようございます。' + EOL + str(datetime.date.today()) + SPACE +'最新の株価情報を送信します。' + EOL + 'ご確認ください。' + EOL +'-----以下、株価情報--------'
 symbol_data_list =define.get_share_info_list(code_list,5,1)
-symbol_data_list.sort(key=lambda x: x["timestamp"])
-print(symbol_data_list)
+# codeでソート
+symbol_data_list.sort(key=lambda x: x["code"])
+#print(symbol_data_list)
 list_size = len(symbol_data_list)
+share_list = []
 for d in range(0,int(list_size)):
 	symbol_data = symbol_data_list[d]
 	price_list = symbol_data["close"]
 	time_list = symbol_data["timestamp"]
-	print(len(time_list))
 	if (d == 0):
 		name = '【4220】リケンテクノス'
 	elif(d == 1):
@@ -46,15 +52,43 @@ for d in range(0,int(list_size)):
 	else:
 		name = '【9432】NTT'
 	message = message + EOL + name
-	for i in range(len(time_list)-2,len(time_list)): # 直近2世代分の株価を取得
-		time = time_list[i]
+
+	# 株価前日比計算用の変数を設定
+	cnt = 0
+	before_price = ''
+
+	# 直近2世代分の株価を取得
+	for i in range(len(time_list)-2,len(time_list)):
 		price = str(price_list[i])
-		time = datetime.utcfromtimestamp(int(time/1000))
-		time = time.strftime("%m/%d")
-		msg_time = '日時' + str(time)
-		msg_price = '株価' + str(price)
-		message1 = msg_time + SPECE + msg_price
-		message = str(message) + EOL + message1
-#print(message)
+		# 小数点第一位で四捨五入
+		price_str = Decimal(str(price)).quantize(Decimal('0.1'), rounding=ROUND_HALF_UP)
+
+		if cnt == 0:
+			before_price = price_str
+		else:
+			msg_price = '株価 ' + '\\' +str(price_str)
+			before_diff = Decimal(price_str) - Decimal(before_price)
+			msg_diff = '前日'+ '\\' + str(before_price) + '(' + '\\' + str(before_diff) + ')'
+			message1 = msg_price + SPACE + msg_diff
+			message = str(message) + EOL + message1
+			share_list.append([name,msg_price,msg_diff])
+		cnt += 1
+
 message = message + EOL + EOL + 'SBI証券ログインページ' + EOL + 'https://www.sbisec.co.jp/ETGate/WPLETmgR001Control?OutSide=on&getFlg=on&burl=search_fx&cat1=fx&cat2=guide&dir=guide&file=fx_guide_02_01.html'
-define.send_line_notify(message)
+
+#株価表の作成
+# 表のサイズ指定
+fig = plt.figure(figsize=(6,4),dpi=150)
+plt.rcParams['font.family'] = 'Meiryo'
+ax1 = fig.add_subplot(111)
+ax1.axis("off")
+columns =['【銘柄コード】企業名','株価','前日']
+ax1.table(cellText=share_list, colLabels=columns,
+        loc='center', bbox=[0, 0, 1, 1])
+#メモリに画像を保存
+buf = io.BytesIO()
+plt.savefig(buf, format='png')
+plt.close()
+
+#LINE通知
+define.send_line_notify(message,buf.getvalue())
